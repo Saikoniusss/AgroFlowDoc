5898ff8949afc76a588e66f5b0f893e592d37a64
Ruslan <kruslan@rocketfirm.com>
2025-11-07 10:42:09 +0500

    (frontend): update router, auth store and layout

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './routers/index'
+import router from './routers/index.js'
 import { createPinia } from 'pinia'
 
 import PrimeVue from 'primevue/config'
--- frontend/src/routers/index.js
+++ frontend/src/routers/index.js
@@ -1,22 +1,80 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LoginView from '@/views/Users/LoginView.vue';
-import RegisterView from '@/views/Users/RegisterView.vue';
-import DocumentList from '@/views/Document/DocumentList.vue';
-import AdminUserConfirm from '@/views/AdminPage/AdminUserConfirm.vue';
-import ProfileView from '@/views/Users/UserProfile.vue';
-
-const routes = [
-  { path: '/', redirect: '/login' },
-  { path: '/login', component: LoginView },
-  { path: '/register', component: RegisterView },
-  { path: '/documents', component: DocumentList },
-  { path: '/admin/users', component: AdminUserConfirm, meta: {requiresAdmin: true} },
-  { path: '/profile', component: ProfileView },
-];
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes: [
+        { 
+            path: '/',
+            redirect: '/login' 
+        },
+        { 
+            path: '/login',
+            component: () => import('../views/Users/LoginView.vue')
+        },
+        { 
+            path: '/register',
+            component: () => import('../views/Users/RegisterView.vue')
+        },
+        { 
+            path: '/admin/users',
+            component: () => import('../views/AdminPage/AdminUserConfirm.vue'),
+            meta: {requiresAdmin: true} 
+        },
+        { 
+            path: '/profile',
+            component: () => import('../views/Users/UserProfile.vue')
+        },
+        {
+            component: () => import('../layouts/Layout.vue'),
+            children: [
+                {
+                    path: '/documents',
+                    name: 'DocumentList',
+                    component: () => import('../views/Document/DocumentList.vue')
+                },
+                {
+                    path: '/application',
+                    name: 'Application',
+                    component: () => import('../views/Application.vue')
+                },
+                {
+                    path: '/invoice',
+                    name: 'Invoice',
+                    component: () => import('../views/Invoice.vue')
+                },
+                {
+                    path: '/receipt-request',
+                    name: 'Receipt Request',
+                    component: () => import('../views/ReceiptRequest.vue')
+                },
+                {
+                    path: '/issue-request',
+                    name: 'Issue Request',
+                    component: () => import('../views/IssueRequest.vue')
+                },
+                {
+                    path: '/export-expense-invoice',
+                    name: 'Export Expense Invoice',
+                    component: () => import('../views/ExportExpenseInvoice.vue')
+                },
+                {
+                    path: '/export-shipments-archive',
+                    name: 'Export Shipments Archive',
+                    component: () => import('../views/ExportShipmentsArchive.vue')
+                },
+                {
+                    path: '/warehouse',
+                    name: 'Warehouse',
+                    component: () => import('../views/Warehouse.vue')
+                },
+                {
+                    path: '/grain-reception',
+                    name: 'Grain Reception',
+                    component: () => import('../views/GrainReception.vue')
+                }
+            ]
+        },
+    ]
 });
 
 export default router;
