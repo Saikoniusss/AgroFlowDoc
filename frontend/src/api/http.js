650409305c77a0759291a908e61f8b053932c183
RuslanKambarov <43203672+RuslanKambarov@users.noreply.github.com>
2025-11-11 12:04:53 +0500

    Revert "(frontend): update router, admin pages"

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,25 +1,12 @@
 import axios from 'axios';
-import router from '../routers';
 
 const http = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'http://localhost:5097/api',
 });
 
 const token = localStorage.getItem('token');
 if (token) {
   http.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
-
-http.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      router.push('/login');
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default http;
